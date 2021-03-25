50f25a5c1b205d1b444d5dd8a0685dfd611ae9ee
Mika Yeap <yeapmika@gmail.com>
2021-03-25 21:59:32 +0800

    dark mode

--- src/components/Navbar.svelte
+++ src/components/Navbar.svelte
@@ -25,7 +25,7 @@
 
 <nav class="navbar navbar-expand-sm sticky-top navbar-light" id="the-nav">
   <div class="container-fluid">
-    <a class="navbar-brand" href="/">RELM</a>
+    <a class="navbar-brand" href="/">Anastasia</a>
     <button
       class="navbar-toggler"
       type="button"
@@ -44,21 +44,21 @@
           <a class="nav-link disabled" disabled>{id ? id : ""}</a>
         </li>
         {#if !id}
-          <li class="nav-item">
+          <!-- <li class="nav-item">
             <a class="nav-link active" href="/">About</a>
-          </li>
+          </li> -->
           <li class="nav-item">
             <a class="nav-link active" href="/">Pricing</a>
           </li>
         {:else}
           <li class="nav-item">
-            <a class="nav-link active" href="/listings/all">All</a>
+            <a class="nav-link active" href="/listings/all">Active</a>
           </li>
           <li class="nav-item">
-            <a class="nav-link active" href="/listings/pending">Pending</a>
+            <a class="nav-link active" href="/listings/pending">All</a>
           </li>
           <li class="nav-item">
-            <a class="nav-link active" href="/add">Add</a>
+            <a class="nav-link active" href="/add">Exchangesk</a>
           </li>
           <li class="nav-item">
             <!-- svelte-ignore a11y-missing-attribute -->
@@ -68,28 +68,7 @@
         <li class="nav-item d-sm-none">
           <span>***</span>
         </li>
-        <!-- <li class="nav-item dropdown">
-                    <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button"
-                        data-bs-toggle="dropdown" aria-expanded="false">
-                        Dropdown
-                    </a>
-                    <ul class="dropdown-menu" aria-labelledby="navbarDropdown">
-                        <li><a class="dropdown-item" href="#">Action</a></li>
-                        <li><a class="dropdown-item" href="#">Another action</a></li>
-                        <li>
-                            <hr class="dropdown-divider">
-                        </li>
-                        <li><a class="dropdown-item" href="#">Something else here</a></li>
-                    </ul>
-                </li> -->
-        <!-- <li class="nav-item">
-                    <a class="nav-link disabled" href="#" tabindex="-1" aria-disabled="true">Disabled</a>
-                </li> -->
       </ul>
-      <!-- <form class="d-flex">
-                <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
-                <button class="btn btn-outline-success" type="submit">Search</button>
-            </form> -->
     </div>
   </div>
 </nav>
@@ -99,7 +78,8 @@
 
   nav {
     font-family: $title-font;
-    background-color: $cream;
+    background-color: $blood;
+    color: $ivory;
 
     margin-bottom: 1rem;
     padding: 0;
@@ -122,10 +102,15 @@
     font-size: 1.5rem;
     padding: 0;
     margin: 1rem 2rem;
+    color: $ivory;
 
     @media only screen and (max-width: 767px) {
       margin: 0.75rem 1rem;
     }
+  }
+
+  .nav-link.active {
+    color: $ivory;
   }
 
   button.navbar-toggler {
@@ -133,7 +118,7 @@
 
     i {
       opacity: 100;
-      color: $blood;
+      color: $cream;
     }
   }
 
