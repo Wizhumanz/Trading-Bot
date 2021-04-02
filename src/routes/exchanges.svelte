b1885f4bdd12053e487ea27a0f4ecede957da91a
Mika Yeap <yeapmika@gmail.com>
2021-04-02 13:47:27 +0800

    basic exchange component + cleanup

---
+++ src/routes/exchanges.svelte
@@ -0,0 +1,104 @@
+<script>
+  import { stores } from "@sapper/app";
+  import { storeUser, currentPage } from "../../store.js";
+
+  import AddBot from "./add.svelte";
+  import BotLI from "../components/ExchangeLI.svelte";
+  import LoadingIndicator from "../components/LoadingIndicator.svelte";
+
+  const { page } = stores();
+  page.subscribe(({ path, params, query }) => {
+    route = params.slug;
+    currentPage.set(route);
+  });
+  var route;
+  let user = {};
+  storeUser.subscribe((newValue) => {
+    if (newValue) {
+      user = JSON.parse(newValue);
+    }
+  });
+
+  let loading = false;
+</script>
+
+<!--Loading Sign-->
+{#if loading}
+  <LoadingIndicator />
+{/if}
+
+<div class="container">
+  {#if route == "active"}
+    <ul id="head">
+      <li><h1>Exchanges</h1></li>
+      <li>
+        <a
+          class="btn"
+          data-bs-toggle="collapse"
+          href="#collapseExample"
+          role="button"
+          aria-expanded="false"
+          aria-controls="collapseExample"
+        >
+          Add
+        </a>
+      </li>
+    </ul>
+  {:else if route == "all"}
+    <h1>All Bots</h1>
+  {/if}
+
+  <div class="collapse" id="collapseExample">
+    <AddBot />
+  </div>
+
+  <div class="botList">
+    {#if user.bots && user.bots.length > 0}
+      {#each user.bots as b}
+        <BotLI bot={b} id={user.id} />
+      {/each}
+    {:else}
+      <p>Error: No bots to show.</p>
+    {/if}
+  </div>
+</div>
+
+<style type="text/scss">
+  @import "../../static/styles/_all";
+
+  #head {
+    li {
+      display: inline;
+
+      .btn {
+        padding: 0 1rem;
+        margin-top: -1rem;
+        color: $cream;
+      }
+
+      h1,
+      a {
+        width: fit-content;
+        display: inline;
+      }
+    }
+  }
+
+  div.container {
+    text-align: center;
+    padding-bottom: 4rem;
+    margin-top: 1rem;
+  }
+
+  div.botList {
+    margin-top: 1rem;
+  }
+
+  button {
+    font-size: x-large;
+  }
+
+  button:disabled {
+    opacity: 0.5;
+  }
+</style>
