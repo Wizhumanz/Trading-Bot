7ac050b95bd9f063f3dabdac1d343fe54022b680
Mika Yeap <yeapmika@gmail.com>
2021-04-13 15:11:36 +0800

    full style

--- src/routes/strategy/index.svelte
+++ src/routes/strategy/index.svelte
@@ -1,4 +1,27 @@
 <script>
+  import { storeAppTheme } from "../../../store.js";
+
+  let appThemeIsDark = false;
+  storeAppTheme.subscribe((newVal) => {
+    appThemeIsDark = newVal === "dark";
+  });
 </script>
 
-<h1>Index route!</h1>
+<div id="stratIndex" class:dark={appThemeIsDark}>
+  <h1>Strat Index!</h1>
+</div>
+
+<style type="text/scss">
+  @import "../../../static/styles/_all";
+
+  #stratIndex {
+    position: absolute;
+    width: 100%;
+    height: 100%;
+  }
+
+  #stratIndex.dark {
+    background-color: black;
+    color: $cream;
+  }
+</style>
