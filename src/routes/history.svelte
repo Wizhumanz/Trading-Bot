3f85356aa8da2bd3e1910b163c31260b028e2ec3
Simon Jeong <simon20000808@gmail.com>
2021-04-13 14:59:26 +0900

    just commiting so I can pull

--- src/routes/history.svelte
+++ src/routes/history.svelte
@@ -2,6 +2,7 @@
   import { storeUser, storeAppTheme } from "../../store.js";
   import axios from "axios";
 
+  let view = "grouped";
   let user = {};
   storeUser.subscribe((newValue) => {
     if (newValue) {
@@ -37,30 +38,6 @@
     .catch((error) => {
       console.log(error.response);
     });
-
-  // const testURL = "https://ana-api.myika.co/trades" + "?user=" + user.id;
-  // const myInit = {
-  //   method: "GET",
-  //   mode: "cors",
-  //   headers: hds,
-  //   cache: "default",
-  // };
-  // fetch(testURL, myInit)
-  //   .then(function (response) {
-  //     if (response.ok) {
-  //       console.log("Success");
-  //     } else {
-  //       console.log("Fail");
-  //     }
-  //     return response.json();
-  //   })
-  //   .then(function (response) {
-  //     user.trades = response;
-  //     storeUser.set(JSON.stringify(user));
-  //   })
-  //   .catch(function (e) {
-  //     console.log(e);
-  //   });
 </script>
 
 <div class="container-fluid">
@@ -106,9 +83,21 @@
     <div class="col-sm-7 col-md-8" />
     <div class="col-sm-4 col-md-3">
       <ul id="viewOptions">
-        <li><a href="/">Log Mode</a></li>
+        <li
+          on:click={() => {
+            view = "log";
+          }}
+        >
+          >Log Mode
+        </li>
         <li>/</li>
-        <li><a href="/">Grouped Mode</a></li>
+        <li
+          on:click={() => {
+            view = "grouped";
+          }}
+        >
+          Grouped Mode
+        </li>
       </ul>
     </div>
   </div>
@@ -127,18 +116,20 @@
       </tr>
     </thead>
     <tbody>
-      {#each user.trades as t}
-        <tr class:dark={appThemeIsDark}>
-          <td>{t.Action}</td>
-          <td>{t.Ticker}</td>
-          <td>{t.Size}</td>
-          <td>{t.Timestamp}</td>
-          <td>{t.BotID}</td>
-          <td>{t.AggregateID}</td>
-          <td>{t.KEY}</td>
-          <td>{t.Exchange}</td>
-        </tr>
-      {/each}
+      {#if view === "log"}
+        {#each user.trades as t}
+          <tr class:dark={appThemeIsDark}>
+            <td>{t.Action}</td>
+            <td>{t.Ticker}</td>
+            <td>{t.Size}</td>
+            <td>{t.Timestamp}</td>
+            <td>{t.BotID}</td>
+            <td>{t.AggregateID}</td>
+            <td>{t.KEY}</td>
+            <td>{t.Exchange}</td>
+          </tr>
+        {/each}
+      {:else if view === "grouped"}{/if}
     </tbody>
   </table>
 </div>
