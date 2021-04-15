9809078d7531df9bf790bb4fff8b49c308886bae
Mika Yeap <yeapmika@gmail.com>
2021-04-15 21:15:09 +0800

    fonts

--- src/components/Navbar.svelte
+++ src/components/Navbar.svelte
@@ -44,8 +44,7 @@
 >
   <div class="container-fluid">
     <a class="navbar-brand {appThemeIsDark === true ? 'dark' : ''}" href="/"
-      >Anastasia</a
-    >
+      >Anastasia.</a>
     <button
       class="navbar-toggler"
       type="button"
