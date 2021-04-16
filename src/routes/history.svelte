186e63b9fb33865a24a1a0e29c193bcc304b0df0
Simon Jeong <simon20000808@gmail.com>
2021-04-17 01:59:51 +0900

    timestamp should display "hours ago"

--- src/routes/history.svelte
+++ src/routes/history.svelte
@@ -6,6 +6,7 @@
   let groupedView = {};
   let numOfTradeAction = {};
   let user = {};
+  let timestampTA = {};
   let whichKey = [];
   let showLong = true;
   let showShort = true;
@@ -24,6 +25,37 @@
     appThemeIsDark = newVal === "dark";
   });
 
+  function timeDiff(curr, prev) {
+    var ms_Min = 60 * 1000; // milliseconds in Minute
+    var ms_Hour = ms_Min * 60; // milliseconds in Hour
+    var ms_Day = ms_Hour * 24; // milliseconds in day
+    var ms_Mon = ms_Day * 30; // milliseconds in Month
+    var ms_Yr = ms_Day * 365; // milliseconds in Year
+    var diff = curr - prev; //difference between dates.
+    // If the diff is less then milliseconds in a minute
+    if (diff < ms_Min) {
+        return Math.round(diff / 1000) + ' seconds ago';
+
+        // If the diff is less then milliseconds in a Hour
+    } else if (diff < ms_Hour) {
+        return Math.round(diff / ms_Min) + ' minutes ago';
+
+        // If the diff is less then milliseconds in a day
+    } else if (diff < ms_Day) {
+        return Math.round(diff / ms_Hour) + ' hours ago';
+
+        // If the diff is less then milliseconds in a Month
+    } else if (diff < ms_Mon) {
+        return 'Around ' + Math.round(diff / ms_Day) + ' days ago';
+
+        // If the diff is less then milliseconds in a year
+    } else if (diff < ms_Yr) {
+        return 'Around ' + Math.round(diff / ms_Mon) + ' months ago';
+    } else {
+        return 'Around ' + Math.round(diff / ms_Yr) + ' years ago';
+    }
+  }
+
   function viewOptionsHandler() {
     //logic for grouped view
     user.trades.forEach((v) => {
@@ -33,7 +65,19 @@
         groupedView[v.AggregateID] = [v];
       }
     })
+
+    //logic for timestamp
+    for (let key in groupedView) {
+      let dict = {}
+      groupedView[key].forEach((v) => {
+        dict[v.Timestamp] = timeDiff(new Date(), new Date(v.Timestamp.replaceAll("_"," ")))
+      })
+      timestampTA[key] = dict
+    }
+    console.log(new Date)
   }
+
+  // Number of trade actions for each view options
   //&& ((v.Direction === "LONG" && showLong) || (v.Direction === "SHORT" && showShort))
   $: for (let key in groupedView) {
     let num = 0;
@@ -59,7 +103,6 @@
   }
 
   function showHideHistoryHandler(aggID) {
-    console.log(aggID);
     if (whichKey.includes(aggID)) {
       delete whichKey[whichKey.indexOf(aggID)];
       whichKey = whichKey;
@@ -225,7 +268,11 @@
                   <td>{t.Action}</td>
                   <td>{t.Ticker}</td>
                   <td>{t.Size}</td>
-                  <td>{t.Timestamp}</td>
+                  {#if timestampTA[t.AggregateID][t.Timestamp].includes("Around")}
+                  <td>{t.Timestamp.substring(0, t.Timestamp.indexOf("+")).replaceAll("_"," ")}</td>
+                  {:else}
+                  <td>{timestampTA[t.AggregateID][t.Timestamp]}</td>
+                  {/if}
                   <td>{t.BotID}</td>
                   <td>{t.AggregateID}</td>
                   <td>{t.Exchange}</td>
@@ -263,7 +310,11 @@
                       <td class="expanded-row">{tradeAction.Action}</td>
                       <td class="expanded-row">{tradeAction.Ticker}</td>
                       <td class="expanded-row">{tradeAction.Size}</td>
+                      {#if timestampTA[key][tradeAction.Timestamp].includes("Around")}
                       <td class="expanded-row">{tradeAction.Timestamp}</td>
+                      {:else}
+                      <td class="expanded-row">{timestampTA[key][tradeAction.Timestamp]}</td>
+                      {/if}
                       <td class="expanded-row">{tradeAction.BotID}</td>
                       <td class="expanded-row">{tradeAction.AggregateID}</td>
                       <td class="expanded-row">{tradeAction.Exchange}</td>
